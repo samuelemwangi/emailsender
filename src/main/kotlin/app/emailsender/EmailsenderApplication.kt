a7bc7de54db037662ec32f6263d21a10ad41c5fc
samuelemwangi <mwangi.s.irungu@gmail.com>
2023-04-02 11:36:57 +0300

    Exclude R2DBC Auto Config

--- src/main/kotlin/app/emailsender/EmailsenderApplication.kt
+++ src/main/kotlin/app/emailsender/EmailsenderApplication.kt
@@ -1,9 +1,10 @@
 package app.emailsender
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
 class EmailsenderApplication
 
 fun main(args: Array<String>) {
