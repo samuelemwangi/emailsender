ad02015affe23ca853631883463edbcbd833fc1a
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-04-14 02:34:45 +0300

    code & test updates

--- src/main/kotlin/app/emailsender/EmailsenderApplication.kt
+++ src/main/kotlin/app/emailsender/EmailsenderApplication.kt
@@ -4,10 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.ServerCodecConfigurer
-import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
 @OpenAPIDefinition
@@ -15,12 +11,4 @@ class EmailsenderApplication
 
 fun main(args: Array<String>) {
     runApplication<EmailsenderApplication>(*args)
-}
-
-@Configuration
-@EnableWebFlux
-class MyConfig : WebFluxConfigurer {
-    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
-        configurer.defaultCodecs().enableLoggingRequestDetails(true)
-    }
 }
